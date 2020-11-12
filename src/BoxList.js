e868535309bc41c8caada7ce258c849be45ff185
Omar6741 <70788409+Omar6741@users.noreply.github.com>
2020-11-12 13:56:27 -0600

    Initial commit

---
+++ src/BoxList.js
@@ -0,0 +1,10 @@
+
+function BoxList(){
+
+    const [makeBox, setMakeBox] = useState(0);
+    const initialState = {height: "", width:"", backgroundColor:""};
+
+
+}
+
+
---
+++ src/NewBoxForm.js
@@ -0,0 +1,27 @@
+function NewBoxForm() {
+
+
+
+
+
+    return 
+    <form>
+
+     <label htmlFor = "height">
+         <input id = "height" value = {formData.height} name = "height"></input>
+     </label>
+
+     <label htmlFor = "width">
+         <input id = "width"value = {formData.width} name = "width"></input>
+     </label>
+
+     <label htmlFor = "background-color">
+         <input id = "background-color" value = {formData.backgroundColor} name = "heibackground-color"></input>
+     </label>
+
+
+
+
+
+    </form>
+}
